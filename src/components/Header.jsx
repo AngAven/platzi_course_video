c40367535d15194f49871dd6c346d06c61eec9c6
Angel <angel.aven.c@gmail.com>
2021-05-05 04:34:09 -0500

    Gravatar terminado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import gravatar from '../utils/gravatar'
 import { Link } from "react-router-dom";
 import  '../assets/styles/components/Header.scss'
-
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import userIcon from '../assets/static/user-icon.png'
 
-const Header = () => (
-    <header className="header">
-        <Link to={"/"}>
-            <img className="header__img" src={logo} alt="Platzi Video" />
-        </Link>
+const Header = props => {
+    const { user } = props
+    const hasUser = Object.keys(user).length > 0
+
+    return(
+        <header className="header">
+            <Link to={"/"}>
+                <img className="header__img" src={logo} alt="Platzi Video"/>
+            </Link>
             <div className="header__menu">
                 <div className="header__menu--profile">
-                    <img src={userIcon} alt="" />
-                        <p>Perfil</p>
+                    {
+                        hasUser
+                        ? <img src={gravatar(user.email)} alt={user.emnail}/>
+                        : <img src={userIcon} alt=""/>
+                    }
+                    <p>Perfil</p>
                 </div>
                 <ul>
                     <li>
@@ -28,7 +37,14 @@ const Header = () => (
                     </li>
                 </ul>
             </div>
-    </header>
-)
+        </header>
+    )
+}
 
-export default Header
+const mapStateToProps = state => {
+    return{
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, null)(Header)
--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -2,7 +2,9 @@ import md5 from 'md5'
 
 const gravatar = (email) => {
     const base = 'https://gravatar.com/avatar'
-    const formatEmail = (email).trim().toLocaleLowerCase()
-    const hash = md5(formatEmail, {encoding: "binary"})
+    const formatedEmail = (email).trim().toLocaleLowerCase()
+    const hash = md5(formatedEmail, {encoding: "binary"})
     return `${base}${hash}`
 }
+
+export default gravatar
